99a3ae44eab3db4326b690ae20ffa8d4a5e45fd1
HouAng <l>
2020-10-02 15:13:22 +0800

    init

---
+++ .history/routes/api/profiles_20200920233159.js
@@ -0,0 +1,45 @@
+//@login & register
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+
+const Profile = require("../../models/Profile");
+
+// $route   GET api/profiles/test
+// @desc    返回请求的json数据
+// @access  public
+router.get("/test",(req,res)=>{
+    res.json({msg:"Profile works"});
+});
+
+// $route   POST api/profiles/add
+// @desc    创建信息接口
+// @access  private
+router.post("/add",passport.authenticate("jwt",{session:false}),(req,res)=>{
+    const profileFields = {};
+    if(req.body.type) profileFields.type = req.body.type;
+    if(req.body.describe) profileFields.describe = req.body.describe;
+    if(req.body.income) profileFields.income = req.body.income;
+    if(req.body.expend) profileFields.expend = req.body.expend;
+    if(req.body.cash) profileFields.cash = req.body.cash;
+    if(req.body.remark) profileFields.remark = req.body.remark;
+    new Profile(profileFields).save().then(profile => {
+        res.json(profile);
+    });
+});
+
+// $route   GET api/profiles
+// @desc    获取所有信息
+// @access  private
+router.get("/",passport.authenticate("jwt",{session:false}),(req,res)=>{
+    new Profile().find()
+    .then(profile => {
+        if(!profile){
+            return res.status(404).json("没有任何内容");
+        }
+        res.json(profile);
+    }).catch(err => res.status(404).json(err));
+});
+
+
+module.exports = router;
